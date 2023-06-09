79503a1a30bb691b0abcd516495505da52e51ec0
Tyrael122 <kanbme@protonmail.com>
2023-06-09 00:38:27 -0300

    Conversão do datagrid de cursos

--- Views/Funcionario/PageCursoFuncionario.xaml.vb
+++ Views/Funcionario/PageCursoFuncionario.xaml.vb
@@ -12,12 +12,12 @@
     End Sub
 
     Private Sub btnCadastroCurso_Click(sender As Object, e As RoutedEventArgs) Handles btnCadastroCurso.Click
-        Dim idsDisciplinasCurso = LoadIdsFromSelectedRows(dataGridDisciplinasCurso)
+        Dim idsDisciplinasCurso = LoadIdsFromSelectedRows(userControlDisciplinas.DataGrid)
 
         Presenter.RegisterCurso(idsDisciplinasCurso)
     End Sub
 
     Private Sub PageCursoFuncionario_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        dataGridDisciplinasCurso.ItemsSource = Presenter.GetDataViewWithCheckboxColumn("Disciplina", False)
+        userControlDisciplinas.DataGrid.ItemsSource = Presenter.GetDataViewWithCheckboxColumn("Disciplina", False)
     End Sub
 End Class
