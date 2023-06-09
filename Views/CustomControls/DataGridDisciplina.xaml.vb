ef26e6cc2d014afb7d11743661306693cef16b19
Tyrael122 <kanbme@protonmail.com>
2023-06-08 23:42:54 -0300

    Resource Dictionary for DataGrid working

---
+++ Views/CustomControls/DataGridDisciplina.xaml.vb
@@ -0,0 +1,10 @@
+﻿Public Class DataGridDisciplina
+    Public Property DataGrid As DataGrid
+        Get
+            Return MainDataGrid
+        End Get
+        Set(value As DataGrid)
+            MainDataGrid = value
+        End Set
+    End Property
+End Class
