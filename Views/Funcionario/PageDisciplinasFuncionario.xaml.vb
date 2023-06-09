b92d57af21d6a1f81c07e03fbc1c8a294de84e6e
Tyrael122 <kanbme@protonmail.com>
2023-06-08 21:10:00 -0300

    Cadastro de disciplina funcionando

--- Views/Funcionario/PageDisciplinasFuncionario.xaml.vb
+++ Views/Funcionario/PageDisciplinasFuncionario.xaml.vb
@@ -10,4 +10,12 @@
     Public Overrides Sub DisplayError()
         Throw New NotImplementedException()
     End Sub
+
+    Private Sub btnCadastrarDisciplina_Click(sender As Object, e As RoutedEventArgs) Handles btnCadastrarDisciplina.Click
+        Presenter.RegisterDisciplina()
+    End Sub
+
+    Private Sub PageDisciplinasFuncionario_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        dataGridDisciplinas.ItemsSource = Presenter.GetDataView("Disciplina")
+    End Sub
 End Class
