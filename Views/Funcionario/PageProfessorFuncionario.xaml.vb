5687d949bcac30b5d0939da537f574b7b8629b20
Tyrael122 <kanbme@protonmail.com>
2023-06-09 00:36:06 -0300

    Conversão do datagrid do professor

--- Views/Funcionario/PageProfessorFuncionario.xaml.vb
+++ Views/Funcionario/PageProfessorFuncionario.xaml.vb
@@ -12,13 +12,13 @@
     End Sub
 
     Private Sub btnCadastrarProfessor_Click(sender As Object, e As RoutedEventArgs) Handles btnCadastrarProfessor.Click
-        Dim idsDisciplinasProfessor = LoadIdsFromSelectedRows(dataGridDisciplinasProfessor)
+        Dim idsDisciplinasProfessor = LoadIdsFromSelectedRows(userControlDisciplinas.DataGrid)
 
         Presenter.RegisterProfessor(idsDisciplinasProfessor)
     End Sub
 
     Private Sub PageProfessorFuncionario_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        dataGridDisciplinasProfessor.ItemsSource = Presenter.GetDataViewWithCheckboxColumn("Disciplina", False)
+        userControlDisciplinas.DataGrid.ItemsSource = Presenter.GetDataViewWithCheckboxColumn("Disciplina", False)
     End Sub
 
     Private Sub btnImage_Click(sender As Object, e As RoutedEventArgs) Handles btnImage.Click
